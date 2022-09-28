145b62715f21e2fd706d4578ebebd15409cf72c0
Eli Pagan <mbk8689@gmail.com>
2022-09-28 13:12:02 -0400

    added IDs so we can manipulate DOM

--- quoteGenerator/script.js
+++ quoteGenerator/script.js
@@ -1,12 +1,18 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
 // Get Quotes From API
 async function getQuotes() {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "47d65fbff9msh0b32e6e97d91b11p165935jsn8a343138cd00",
-      "X-RapidAPI-Host": "inspiring-quotes.p.rapidapi.com",
-    },
-  };
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '47d65fbff9msh0b32e6e97d91b11p165935jsn8a343138cd00',
+            'X-RapidAPI-Host': 'inspiring-quotes.p.rapidapi.com'
+        }
+    };
   await fetch("https://inspiring-quotes.p.rapidapi.com/random?author", options)
     .then((response) => response.json())
     .then((response) => console.log(response))
