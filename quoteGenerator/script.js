fc010823dae346595c037b0accb0474ea6fa0812
Eli Pagan <mbk8689@gmail.com>
2022-09-28 11:42:47 -0400

    set up apiUrl in script.js

---
+++ quoteGenerator/script.js
@@ -0,0 +1,9 @@
+// Get Quotes From API
+async function getQuotes() {
+  const apiUrl = "https://inspiring-quotes.p.rapidapi.com/random";
+  try {
+    const response = await fetch(apiUrl);
+  } catch (error) {
+    // Catch Error Here
+  }
+}
