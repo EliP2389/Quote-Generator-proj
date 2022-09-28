71e7b8626b02284947314386adf83bbb1135d13d
Eli Pagan <mbk8689@gmail.com>
2022-09-28 12:30:52 -0400

    added getQuotes function that generates random quotes

--- quoteGenerator/script.js
+++ quoteGenerator/script.js
@@ -1,9 +1,17 @@
 // Get Quotes From API
 async function getQuotes() {
-  const apiUrl = "https://inspiring-quotes.p.rapidapi.com/random";
-  try {
-    const response = await fetch(apiUrl);
-  } catch (error) {
-    // Catch Error Here
-  }
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "47d65fbff9msh0b32e6e97d91b11p165935jsn8a343138cd00",
+      "X-RapidAPI-Host": "inspiring-quotes.p.rapidapi.com",
+    },
+  };
+  await fetch("https://inspiring-quotes.p.rapidapi.com/random?author", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err))
 }
+
+// On Load
+getQuotes();
