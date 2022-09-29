c333134dc9e26d2f23c0fced7e5bc3e6ab3f42d6
Eli Pagan <mbk8689@gmail.com>
2022-09-29 08:35:06 -0400

    created a loading function and complete function in js file

--- quoteGenerator/script.js
+++ quoteGenerator/script.js
@@ -3,6 +3,19 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Show loading
+loading = () => {
+loader.hidden = false;
+quoteContainer.hidden = true;
+}
+
+// Hide loading
+complete = () => {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // Get Quotes From API
 async function getQuotes() {
